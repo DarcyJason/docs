6357e7d5c39bbc58005e40497f370925c0961282
NotNone <yuzhiqiang508384097@gmail.com>
2025-02-12 14:54:02 +0800

    update config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
 		starlight({
 			title: 'Homeryland Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/DarcyJason/docs',
 			},
 			sidebar: [
 				{
